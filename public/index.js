57ced0fb63ebab8df9307d0aae14248b6559e0d0
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-03-18 09:11:15 +0300

    First commit

---
+++ public/index.js
@@ -0,0 +1,7 @@
+var sport = 'football';
+var id = 5;
+// Functions
+function circle(d) {
+    return 'The circumference is ' + Math.PI * d;
+}
+console.log(circle(12));
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let sport = 'football';
+let id = 5;
+
+
+// Functions
+function circle(d: number): string {
+    return 'The circumference is ' + Math.PI * d
+}
+
+console.log(circle(12));
