d46aaf5cefd774fff3716699c3fc086de91ad6f8
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-03-18 10:55:45 +0300

    Protect values with public, private and readonly

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
-
-  constructor(c: string, d: string, a: number){
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
 
   format() {
-    return `${this.client} owes KSH${this.amount} for ${this.details}`
+    return `${this.client} owes KSH${this.amount} for ${this.details}`;
   }
 }
-
